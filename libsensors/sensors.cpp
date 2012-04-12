d1e7e8b4fd3cf7d1b9a71006ac8addb78825a070
Cass <getcass@gmail.com>
2012-04-13 00:45:06 +0100

    Updated libsensors

--- libsensors/sensors.cpp
+++ libsensors/sensors.cpp
@@ -1,32 +1,29 @@
 /*
-* Copyright (C) 2011 Eduardo José Tagle
-* Copyright (C) 2010 Danijel Posilovic - dan1j3l
-* Copyright (C) 2008 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2011 Eduardo José Tagle
+ * Copyright (C) 2010 Danijel Posilovic - dan1j3l
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #define LOG_TAG "Sensors"
-
-#define LOG_NDEBUG 1
+//#define LOG_NDEBUG 1
 
 #include <stdint.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <dirent.h>
 #include <math.h>
-//#include <poll.h>
-//#include <pthread.h>
 #include <sys/cdefs.h>
 #include <sys/types.h>
 #include <sys/select.h>
@@ -34,7 +31,6 @@
 
 #include <hardware/hardware.h>
 #include <hardware/sensors.h>
-//#include <cutils/atomic.h>
 #include <cutils/log.h>
 
 /*****************************************************************************/
@@ -46,6 +42,8 @@
 struct sensors_poll_context_t {
 	struct sensors_poll_device_t device; // must be first
 	int fd;
+	int in_use;	/* if sensor is in use right now */
+	int enabled;	/* If sensor is enabled or not */
 };
 
 /*****************************************************************************/
@@ -69,7 +67,7 @@ struct sensors_poll_context_t {
 
 static int open_inputdev(const char* id)
 {
-	/* scan all input drivers and look for "compass" */
+	/* scan all input drivers and look for "id" */
 	int fd = -1;
 	const char *dirname = "/dev/input";
 	char devname[PATH_MAX];
@@ -93,6 +91,7 @@ static int open_inputdev(const char* id)
 		
 		strcpy(filename, de->d_name);
 		
+		LOGD("Querying '%s' ...",devname);
 		fd = open(devname, O_RDONLY);
 		if (fd >= 0) {
 			
@@ -100,6 +99,7 @@ static int open_inputdev(const char* id)
 			if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) < 1) {
 				name[0] = '\0';
 			}
+			LOGD("Device '%s' is '%s' ... Looking for '%s' ...", devname,name,id);
 			if (!strcmp(name, id)) {
 				LOGV("using (name=%s)", name);
 				break;
@@ -116,18 +116,58 @@ static int open_inputdev(const char* id)
 	return fd;
 }
 
+static int inc_uses(sensors_poll_context_t* ctx)
+{
+	if (ctx->in_use == 0) {
+		/* Try the original name */
+		ctx->fd = open_inputdev("bma150");
+	
+		/* If we failed, try the alternate name */
+		if (ctx->fd == -1)
+		    ctx->fd = open_inputdev("accelerometer_tegra");
+	    
+		if (ctx->fd == -1)
+		    return -1;
+	}
+	
+	/* Flag the sensor is in use */
+	ctx->in_use++;
+	return 0;
+}
+
+static int dec_uses(sensors_poll_context_t* ctx)
+{
+	if (ctx->in_use == 1) {
+		close(ctx->fd);
+		ctx->fd = -1;
+	}
+	ctx->in_use --;
+	return 0;
+}
+
 /*****************************************************************************/
 
 static int poll__activate(struct sensors_poll_device_t *dev, int handle, int enabled)
 {
 	sensors_poll_context_t *ctx = (sensors_poll_context_t *)dev;
-	if (ctx->fd == -1 && enabled) {
-		ctx->fd = open_inputdev("bma150");
-	} else
-	if (ctx->fd != -1 && !enabled) {
-		close(ctx->fd);
-		ctx->fd = -1;
+	
+	LOGD("activate: %d",enabled);
+	
+	/* If already in the right state, don't do anything */
+	if (ctx->enabled == enabled)
+	    return 0;
+	
+	/* Enable or disable based on the request */
+	if (enabled) {
+		if (inc_uses(ctx) != 0)
+		    return -1;
+	} else {
+		if (dec_uses(ctx) != 0)
+		    return -1;
 	}
+	
+	/* Store the new state */
+	ctx->enabled = enabled;
 	
 	return 0;
 }
@@ -137,6 +177,9 @@ static int poll__activate(struct sensors_poll_device_t *dev, int handle, int ena
 static int poll__close(struct hw_device_t *dev)
 {
 	struct sensors_poll_context_t* ctx = (struct sensors_poll_context_t*)dev;
+	
+	LOGD("close");
+	
 	if (ctx) {
 		if (ctx->fd != -1) {
 			close(ctx->fd);
@@ -152,9 +195,19 @@ static int poll__close(struct hw_device_t *dev)
 static int poll__poll(struct sensors_poll_device_t *dev, sensors_event_t* data, int count)
 {
 	sensors_poll_context_t *ctx = (sensors_poll_context_t *)dev;
-	int fd = ctx->fd;
+	
+//comment out LOGD to stop span in logs, dont need to see poll every second -cass
+	//LOGD("poll");
+	
+	int fd;
 	int pos = 0;
-
+	
+	if (inc_uses(ctx) != 0) {
+	    LOGE("Unable to enable sensor");
+	    return -1;
+	}
+	
+	fd = ctx->fd;
 	if (fd < 0) {
 		LOGE("invalid accelerometer file descriptor, fd=%d", fd);
 		return -1;
@@ -179,6 +232,7 @@ static int poll__poll(struct sensors_poll_device_t *dev, sensors_event_t* data, 
 		if (n < 0) {
 			LOGE("%s: error from select(%d): %s",
 			__FUNCTION__, fd, strerror(errno));
+			dec_uses(ctx);
 			return pos;
 		}
 
@@ -210,6 +264,7 @@ static int poll__poll(struct sensors_poll_device_t *dev, sensors_event_t* data, 
 					data[pos].timestamp = event.time.tv_usec * 1000; // ns
 					pos++;
 					if (pos >= count)
+					dec_uses(ctx);
 					return pos;
 				}
 			}
@@ -223,6 +278,7 @@ static int poll__poll(struct sensors_poll_device_t *dev, sensors_event_t* data, 
 
 static int poll__set_delay(struct sensors_poll_device_t *dev, int handle, int64_t us)
 {
+	LOGD("set_delay");
 	return 0;
 }
 
@@ -232,6 +288,9 @@ static int poll__set_delay(struct sensors_poll_device_t *dev, int handle, int64_
 static int open_sensors(const struct hw_module_t* module, const char* name, struct hw_device_t** device)
 {
 	struct sensors_poll_context_t *dev;
+	
+	LOGD("open_sensors");
+	
 	dev = (struct sensors_poll_context_t *) malloc(sizeof(*dev));
 	memset(dev, 0, sizeof(*dev));
 	
@@ -242,7 +301,7 @@ static int open_sensors(const struct hw_module_t* module, const char* name, stru
 	dev->device.common.module 	= const_cast<hw_module_t*>(module);;
 	dev->device.common.close  	= poll__close;
 	dev->device.activate 		= poll__activate;
-	dev->device.poll 			= poll__poll;
+	dev->device.poll 		= poll__poll;
 	dev->device.setDelay		= poll__set_delay;
 	*device = &dev->device.common;
 	return 0;
@@ -264,6 +323,8 @@ static const struct sensor_t sSensorList[] = {
 
 static int sensors__get_sensors_list(struct sensors_module_t* module, struct sensor_t const** list)
 {
+	LOGD("get_sensors_list");
+	
 	*list = sSensorList;
 	return ARRAY_SIZE(sSensorList);
 }
@@ -272,7 +333,8 @@ static struct hw_module_methods_t sensors_module_methods = {
 	open: open_sensors,
 };
 
-const struct sensors_module_t HAL_MODULE_INFO_SYM = {
+/* Must not be const, or the linker will throw the symbol! */
+struct sensors_module_t HAL_MODULE_INFO_SYM = {
 	common: {
 		tag: HARDWARE_MODULE_TAG,
 		version_major: 1,
